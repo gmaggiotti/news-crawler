c3928cfd4b53e0c66221023e70263416efedc040
gabe <gmaggiotti@gmail.com>
2018-08-02 14:17:43 -0300

    adding ib

--- src/google_news.js
+++ src/google_news.js
@@ -9,10 +9,13 @@ exports.feeds =[
     {brand: {url:'https://tn.com.ar/feeds/google/newsstand/salud.xml',main:'https://www.tn.com.ar/' ,links:'result.rss.channel[0].item', element:'element.guid[0]._',date:'element.pubDate[0]', scraping:{title:'.article__title', dropline:'.article__dropline', body: '.article__body p'}, site:'TN'}},
     {brand: {url:'https://tn.com.ar/feeds/google/newsstand/tecno.xml',main:'https://www.tn.com.ar/' ,links:'result.rss.channel[0].item', element:'element.guid[0]._',date:'element.pubDate[0]', scraping:{title:'.article__title', dropline:'.article__dropline', body: '.article__body p'}, site:'TN'}},
     {brand: {url:'https://tn.com.ar/feeds/google/newsstand/autos.xml',main:'https://www.tn.com.ar/' ,links:'result.rss.channel[0].item', element:'element.guid[0]._',date:'element.pubDate[0]', scraping:{title:'.article__title', dropline:'.article__dropline', body: '.article__body p'}, site:'TN'}},
-    {brand: {url:'https://tn.com.ar/feeds/google/newsstand/opinion.xml',main:'https://www.tn.com.ar/' ,links:'result.rss.channel[0].item', element:'element.guid[0]._',date:'element.pubDate[0]', scraping:{title:'.article__title', dropline:'.article__dropline', body: '.article__body p'}, site:'TN'}},*/
+    {brand: {url:'https://tn.com.ar/feeds/google/newsstand/opinion.xml',main:'https://www.tn.com.ar/' ,links:'result.rss.channel[0].item', element:'element.guid[0]._',date:'element.pubDate[0]', scraping:{title:'.article__title', dropline:'.article__dropline', body: '.article__body p'}, site:'TN'}},
 
 
-    {brand: {url:'https://www.lanacion.com.ar/sitemap-news.xml', main:'https://www.lanacion.com/' ,links:'result.urlset.url', element:'element.loc[0]', date:'element["news:news"]["0"]["news:publication_date"]["0"]', scraping:{title:'.titulo', dropline:'.epigrafe', body: '#cuerpo p'}, site:'LN'}},
+    {brand: {url:'https://www.lanacion.com.ar/sitemap-news.xml', main:'https://www.lanacion.com/' ,links:'result.urlset.url', element:'element.loc[0]', date:'element["news:news"]["0"]["news:publication_date"]["0"]', scraping:{title:'.titulo', dropline:'.epigrafe', body: '#cuerpo p'}, site:'LN'}},*/
+
+    {brand: {url:'https://www.infobae.com/news_sitemap.xml', main:'https://www.infobae.com/' ,links:'result.urlset.url', element:'element.loc[0]', date:'element["news:news"]["0"]["news:publication_date"]["0"]', scraping:{title:'.article-header', dropline:'.subheadline', body: '#article-body p'}, site:'IB'}}
+
 
 
 ]
